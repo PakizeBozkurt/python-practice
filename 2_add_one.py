5f7caf566d4cc0be115b63295556b4f0656ccd2c
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-28 10:30:15 +0100

    Comments

--- 2_add_one.py
+++ 2_add_one.py
@@ -1,3 +1,8 @@
 def add_one(num):
     return num + 1
 print("add_one(6) returns:")
+print(add_one(6))
+
+# `add_one` is a function (little machine) that takes a
+# number as an input parameter, adds one to it, and then
+# returns the result.
