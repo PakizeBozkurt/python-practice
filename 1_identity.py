91f951b1e6c8aabfb09c0e4214eb5be9f38a1b2b
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-28 10:29:10 +0100

    Comments

--- 1_identity.py
+++ 1_identity.py
@@ -2,3 +2,15 @@ def just_return_it(num):
   return num
 
 just_return_it(4)
+
+# This `just_return_it` function has:
+
+# * A name: `just_return_it` that we can use to call it
+
+# * A parameter: `num` that it takes as input. You might
+#   also hear these informally referred to as 'arguments'
+
+# * A body: `return num` that processes the input and
+#   returns the result as output. Python uses spaces at the
+#   start of each line to indicate what's the body and what
+#   isn't
