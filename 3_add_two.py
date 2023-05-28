d48626664854137b454b10da1e182a3af4501242
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-28 10:32:28 +0100

    add two

---
+++ 3_add_two.py
@@ -0,0 +1,21 @@
+# @TASK: Write a function called `add_two` that:
+#
+# * Takes a number as input
+# * Adds two to it
+# * Returns the result
+
+def add_two(num):
+  return num + 2
+
+
+add_two(6)
+
+print(add_two(6))
+
+
+# print("Function: add_two")
+
+# check_that_these_are_equal(
+#     add_two(6),
+#     8
+# )
