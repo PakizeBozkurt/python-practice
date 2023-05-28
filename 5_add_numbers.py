ccbbba7fc63653ce52de0ee5e73ccf91fec29c06
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-28 10:37:54 +0100

    two parameters

---
+++ 5_add_numbers.py
@@ -0,0 +1,28 @@
+# Write a function called `add_numbers` that:
+#
+# * Takes two numbers as input
+# * Adds them together
+# * Returns the result
+
+
+def add_numbers(num_1, num_2):
+  return num_1 + num_2
+
+
+print("add_numbers(2, 3) is:")
+
+# Below is a test the function.
+
+# print("add_numbers(2, 3) is:")
+
+# check_that_these_are_equal(
+#     add_numbers(2, 3),
+#     5
+# )
+
+# print("add_numbers(3, 5) is:")
+
+# check_that_these_are_equal(
+#     add_numbers(3, 5),
+#     8
+# )
