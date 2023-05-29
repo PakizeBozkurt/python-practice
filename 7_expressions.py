ed928c58f155f2455e52ae200b775410ed6375e2
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-29 22:19:27 +0100

    expressions

---
+++ 7_expressions.py
@@ -0,0 +1,19 @@
+added = 2 + 3
+# The expression part is this:
+
+2 + 3
+
+# The expression is the fundamental unit of computation in your program. It is the combination of data and operators
+# (and some other things) to produce a result.
+
+
+def add_one(num):
+  return num + 1
+
+# Here are some more expressions:
+
+
+add_one(2)          # Evaluates to 3
+add_one(3)          # Evaluates to 4
+add_one(4) * 3      # Evaluates to 15
+2 + add_one(4) * 3  # Evaluates to 17
