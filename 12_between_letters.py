17def21edb46952530cdc302912c2dba79054d2a
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-30 23:47:04 +0100

    Find between letters

---
+++ 12_between_letters.py
@@ -0,0 +1,15 @@
+def get_letters_between_four_and_eight(the_str):
+  return the_str[4:8]
+
+
+# check_that_these_are_equal(
+#     get_letters_between_four_and_eight("The king granted them"),
+#     "king"
+# )
+
+# check_that_these_are_equal(
+#     get_letters_between_four_and_eight("Five years later"),
+#     " yea"
+# )
+print("")
+print("Function: get_letters_between_four_and_eight")
