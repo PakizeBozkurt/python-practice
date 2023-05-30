a6a24cea04b253b771568e70631ffd8330d4c63e
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-30 23:50:02 +0100

    Make lower case

---
+++ 14_make_lower_case.py
@@ -0,0 +1,11 @@
+def make_lowercase(string):
+  return string.lower()
+
+
+# check_that_these_are_equal(
+#     make_lowercase("HELLO"), "hello")
+
+# check_that_these_are_equal(
+#     make_lowercase("World"), "world")
+print("")
+print("Function: lowercase")
