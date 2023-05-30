3f8008e49b8b57affbbd9923e3552d32113ba4b1
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-30 23:44:51 +0100

    Find last letter

---
+++ 11_last_letter.py
@@ -0,0 +1,15 @@
+def get_last_letter(the_str):
+ return the_str[-1]
+
+
+# check_that_these_are_equal(
+#     get_last_letter("The king granted them"),
+#     "m"
+# )
+
+# check_that_these_are_equal(
+#     get_last_letter("Five years later"),
+#     "r"
+# )
+print("")
+print("Function: get_last_letter")
