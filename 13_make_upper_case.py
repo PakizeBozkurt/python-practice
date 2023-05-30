ddd428ce9312838550e20ff9c8f4bdb5f56479d8
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-30 23:48:47 +0100

    Make upper case

---
+++ 13_make_upper_case.py
@@ -0,0 +1,13 @@
+def make_uppercase(string):
+
+  return string.upper()
+
+
+# check_that_these_are_equal(
+#     make_uppercase("hello"), "HELLO")
+
+# check_that_these_are_equal(
+#     make_uppercase("World"), "WORLD")
+
+print("")
+print("Function: uppercase")
