b868a6d7ce1ad92e0e88b0e1ffaae0563885b7c3
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-30 23:44:03 +0100

    Find first letter

---
+++ 10_first_letter.py
@@ -0,0 +1,16 @@
+def get_first_letter(the_str):
+
+  return the_str[0]
+
+
+# check_that_these_are_equal(
+#     get_first_letter("The king granted them"),
+#     "T"
+# )
+
+# check_that_these_are_equal(
+#     get_first_letter("Five years later"),
+#     "F"
+# )
+print("")
+print("Function: get_first_letter")
