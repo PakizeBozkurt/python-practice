ea10c48ccf3524d958e3271aaee4d89cb3237dfe
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-31 20:32:45 +0100

    Trim spaces

---
+++ 15_strip_space.py
@@ -0,0 +1,12 @@
+def strip_whitespace(string):
+ return string.strip()
+
+
+# check_that_these_are_equal(
+#     strip_whitespace("hello "), "hello")
+
+# check_that_these_are_equal(
+#     strip_whitespace(" hello world "), "hello world")
+
+print("")
+print("Function: strip_whitespace")
