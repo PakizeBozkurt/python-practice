3d284e2e939a8cb769c5cebafda7d380e8a225a3
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-31 20:38:24 +0100

    comparations

---
+++ 17_comparations.py
@@ -0,0 +1,21 @@
+
+def a_is_less_than_or_equal_to_b(a, b):
+  return a <= b
+
+
+# check_that_these_are_equal(
+#     a_is_less_than_or_equal_to_b(1, 2),
+#     True
+# )
+
+# check_that_these_are_equal(
+#     a_is_less_than_or_equal_to_b(1, 1),
+#     True
+# )
+
+# check_that_these_are_equal(
+#     a_is_less_than_or_equal_to_b(2, 1),
+#     False
+# )
+print("")
+print("Function: a_is_less_than_or_equal_to_b")
