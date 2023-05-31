e300ea3b4028ef1c3f7caab3204240aa8c4ca5dc
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-31 20:35:52 +0100

    ifs

--- 16_ifs.py
+++ 16_ifs.py
@@ -17,5 +17,38 @@ def is_first_of_the_month(day_number):
 #     "Not first of the month"
 # )
 
+
+
+
+def has_five_chars(the_str):
+  # Return "STRING is five characters long" if the string
+  # is five characters long.
+  # Otherwise, return "Not five characters".
+  if len(the_str) == 5:
+    return f"{the_str} is five characters long"
+  else:
+    return "Not five characters"
+
+
+# check_that_these_are_equal(
+#     has_five_chars("ABCDE"),
+#     "ABCDE is five characters long"
+# )
+
+# check_that_these_are_equal(
+#     has_five_chars("FORGE"),
+#     "FORGE is five characters long"
+# )
+
+# check_that_these_are_equal(
+#     has_five_chars("Nope"),
+#     "Not five characters"
+# )
+
+# check_that_these_are_equal(
+#     has_five_chars("Nor this one"),
+#     "Not five characters"
+# )
+
 print("")
 print("Function: is_first_of_the_month")
