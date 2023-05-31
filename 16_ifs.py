9deae7b4ce7aa7422d1f90ebd0a25932e1a63242
Pakize Bozkurt <pakiozihak@gmail.com>
2023-05-31 20:34:45 +0100

    ifs

---
+++ 16_ifs.py
@@ -0,0 +1,21 @@
+
+def is_first_of_the_month(day_number):
+
+ if day_number == 1:
+   return "First of the month!"
+ else:
+   return "Not first of the month"
+
+
+# check_that_these_are_equal(
+#     is_first_of_the_month(1),
+#     "First of the month!"
+# )
+
+# check_that_these_are_equal(
+#     is_first_of_the_month(12),
+#     "Not first of the month"
+# )
+
+print("")
+print("Function: is_first_of_the_month")
