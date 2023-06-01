90721d4dd4bd713602a224205aab4a83d386664f
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-01 22:48:02 +0100

    logic

---
+++ 18_logic.py
@@ -0,0 +1,11 @@
+def a_and_b(a, b):
+   return a and b
+
+
+# check_that_these_are_equal(a_and_b(True, True), True)
+# check_that_these_are_equal(a_and_b(True, False), False)
+# check_that_these_are_equal(a_and_b(False, True), False)
+# check_that_these_are_equal(a_and_b(False, False), False)
+
+print("")
+print("Function: a_and_b")
