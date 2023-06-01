5212ad7338ca28f1c5b209ac33fe4eebd66c5967
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-01 22:50:04 +0100

    list indexing

---
+++ 19_list_indexing.py
@@ -0,0 +1,18 @@
+
+def get_first_item(the_list):
+  # Return the first item of the list
+ return the_list[0]
+
+
+# check_that_these_are_equal(
+#     get_first_item(["a", "b", "c", "d", "e"]),
+#     "a"
+# )
+
+# check_that_these_are_equal(
+#     get_first_item([34, 44, 54, 64]),
+#     34
+# )
+
+print("")
+print("Function: get_first_item")
