6dcd644d8070f3801d8cf664e5dcf18922a0b8c4
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-02 23:46:30 +0100

    list index modification

--- 20_list_modification.py
+++ 20_list_modification.py
@@ -11,3 +11,18 @@ def remove_item_from_list(the_list, item):
 #     remove_item_from_list(['a', 'b'], 'b'), ['a'])
 # check_that_these_are_equal(
 #     remove_item_from_list([3, 1], 3), [1])
+
+######
+
+print("")
+print("Function: count_items_in_list")
+
+
+def count_items_in_list(the_list, item):
+  return the_list.count(item)
+
+
+# check_that_these_are_equal(
+#     count_items_in_list(['a', 'b', 'a'], 'a'), 2)
+# check_that_these_are_equal(
+#     count_items_in_list([4, 1, 4, 4], 4), 3)
