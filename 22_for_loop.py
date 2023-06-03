c3c306f725c724e30c09fd50600b5837e73e6e89
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-03 22:26:36 +0100

    for loop

--- 22_for_loop.py
+++ 22_for_loop.py
@@ -4,3 +4,15 @@ def print_numbers_in_range():
 
 
 print_numbers_in_range()
+
+
+###while version
+
+def print_numbers_in_range_with_a_while():
+  number = 0
+  while number < 10:
+    print(f"This number is {number}")
+    number = number + 1
+
+
+print_numbers_in_range_with_a_while()
