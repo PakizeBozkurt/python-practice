60dc39a560f688e2e16ddbb28c81ceb4cbfb5d97
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-04 22:35:25 +0100

    filtering

--- 25_filtering.py
+++ 25_filtering.py
@@ -9,3 +9,25 @@ for age in raw_ages:  # We go through each age
 
 print(raw_ages)
 print(clean_ages)
+
+
+####
+
+
+
+def only_positive_numbers(numbers):
+  clean_negs = []
+
+  for number in clean_negs:
+    if number > 0:
+     clean_negs.append(number)
+
+  return clean_negs
+
+
+print("")
+print("Function: only_positive_numbers")
+# check_that_these_are_equal(
+#     only_positive_numbers([-4, 4, -3, 3]), [4, 3])
+# check_that_these_are_equal(
+#     only_positive_numbers([-100]), [])
