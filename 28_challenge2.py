e6181bf1ec7e1f6f7c2bf3f46a033f97273efada
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-05 22:11:22 +0100

    challenge

---
+++ 28_challenge2.py
@@ -0,0 +1,16 @@
+# Write a function that takes a list of words and returns a
+# report of all the words that are longer than 10 characters
+# but don't contain a hyphen. If those words are longer than
+# 15 characters, then they should be shortened to 15
+# characters and have an ellipsis (...) added to the end.
+
+# For example, if the input is:
+# [
+#   'hello',
+#   'nonbiological',
+#   'Kay',
+#   'this-is-a-long-word',
+#   'antidisestablishmentarianism'
+# ]
+# then the output should be:
+# "These words are quite long: nonbiological, antidisestablis..."
