273d7d116eaeeeafd8624fd8a809880e94144713
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-12 23:00:42 +0100

    f-string

--- f-string.py
+++ f-string.py
@@ -17,3 +17,13 @@ str_val = 'apples'
 num_val = 42
 
 print(f'{str_val=}, {num_val = }')  # str_val='apples', num_val = 42
+
+
+# Mathematical operations
+#Not syntactically unlike variable names, you can also perform mathematical operations in f-strings. 
+# #You can place the mathematical expression inside the curly braces and , 
+# #if you add an equal sign, you'll get the expression and its result.
+
+num_val = 42
+
+print(f'{num_val % 2 = }')  # num_val % 2 = 0
