2269ccdc3614acd3a361de98ec1d2840ac4d4478
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-12 22:58:45 +0100

    f-string

---
+++ f-string.py
@@ -0,0 +1,8 @@
+# String Interpolation
+#The most used f-string feature by far is string interpolation. 
+# #All you need to do is wrap the value or variable in curly braces({}) and you're good to go.
+
+str_val = 'apples'
+num_val = 42
+
+print(f'{num_val} {str_val}')  # 42 apples
