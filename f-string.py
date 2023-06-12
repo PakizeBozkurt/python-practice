05923c2a189f895e952e0e725af8738525f83b20
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-12 22:59:58 +0100

    f-string

--- f-string.py
+++ f-string.py
@@ -6,3 +6,14 @@ str_val = 'apples'
 num_val = 42
 
 print(f'{num_val} {str_val}')  # 42 apples
+
+#Variable names
+#Apart from getting a variable's value, you can also get its name alongside the value. 
+# #This can be especially useful when debugging and can be easily accomplished by adding an equals sign(=) after the variable name inside the curly braces.
+
+#Bear in mind that whitespace inside the curly braces is taken into account, so adding spaces around the equals sign can make for a more readable result.
+
+str_val = 'apples'
+num_val = 42
+
+print(f'{str_val=}, {num_val = }')  # str_val='apples', num_val = 42
