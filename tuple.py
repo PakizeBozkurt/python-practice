9b86e120af1abdecd2345fba84b078425ea74682
Pakize Bozkurt <pakiozihak@gmail.com>
2023-06-14 22:59:17 +0100

    digitise

--- tuple.py
+++ tuple.py
@@ -6,3 +6,16 @@ hex_to_rgb('FFA501')  # (255, 165, 1)
 
 #Use a list comprehension in combination with int() and list slice notation to get the RGB components from the hexadecimal string.
 #Use tuple() to convert the resulting list to a tuple.
+
+#######
+
+
+def digitize(n):
+  return list(map(int, str(n)))
+
+
+digitize(123)  # [1, 2, 3]
+
+# Converts a number to a list of digits.
+
+#Use map() combined with int on the string representation of n and return a list from the result
